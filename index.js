5ef0bd72e10e8a7855c5de0353f409364e9e0c30
Jaron Proehl <jaron@strategicmissionelements.com>
2023-12-13 19:05:35 -0500

    2nd attempt

--- index.js
+++ index.js
@@ -14,12 +14,13 @@ const addPartyDetailsForm = document.querySelector("#addPartyDetails");
 addPartyDetailsForm.addEventListener("submit", addParty);
 
 // Sync the state with the API, then render.
-async function render() {
+async function render() { 
+//  debugger;
     await getParties();
     renderParties();    
 }
-render();
 
+render();
 // Update the state with parties from the API
 async function getParties() {
     try {
@@ -44,21 +45,25 @@ function renderParties() {
       li.innerHTML = `
         <h2>${party.name}</h2>
         <h2>${party.date}</h2>
-        <h2>${party.time}</h2>
         <h2>${party.location}</h2>
-        <img src="${party.imageUrl}" alt="${party.name}" />
         <p>${party.description}</p>
+        <button class="delete-button" data-party-id="${party.id}">Delete</button>
       `;
       return li;
     });
 
     partyList.replaceChildren(...partyCards);
 
+    // Addding listeners for delete buttons.
+    const deleteButtons = document.querySelectorAll("delete-button");
+    deleteButtons.forEach((button) => {
+        button.addEventListener("click", () => DeleteParty(button.dataset.partyId));
+    });
 }
 
 // Ask the API to create a new party based on the form data
 async function addParty(event) {
-    event.preventdefault();
+    event.preventDefault();
 
   try {
     const response = await fetch(API_URL, {
@@ -67,10 +72,10 @@ async function addParty(event) {
         body: JSON.stringify({
             name: addPartyDetailsForm.name.value,
             dates: addPartyDetailsForm.dates.value,
-         // times: addPartyDetailsForm.times.value,
+        //  times: addPartyDetailsForm.times.value,
             times: "2021-09-30T00:00:000Z",
             locations: addPartyDetailsForm.locations.value,
-            imageUrl: addPartyDetailsForm.imageUrl.value,
+        //    imageUrl: addPartyDetailsForm.imageUrl.value,
             description: addPartyDetailsForm.description.value,
         }),
     });
@@ -79,7 +84,7 @@ async function addParty(event) {
         throw new Error("Failed to create Party!");
     }
 
-    render();
+     render();
    } catch (error) {
      console.error.apply(error);
    }
