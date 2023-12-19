c502760e87ff27879b1a0b0e5635d7a08f179fab
Jaron Proehl <jaron@strategicmissionelements.com>
2023-12-18 20:30:24 -0500

    fixed date issue

--- index.js
+++ index.js
@@ -9,18 +9,14 @@ const state = {
 // Create a variable called 'partyList' with the id 'parties' to be used as a container for displaying the party details.
 const partyList = document.querySelector("#parties");
 
-// Select the HTML form element with the id "addPartyDetails" and assign it to the variable addPartyDetailsForm. This form is used for adding new party details.
-const addPartyDetailsForm = document.querySelector("#addPartyDetails");
-addPartyDetailsForm.addEventListener("submit", addParty);
-
 // Sync the state with the API, then render.
 async function render() { 
 //  debugger;
     await getParties();
     renderParties();    
 }
-
 render();
+
 // Update the state with parties from the API
 async function getParties() {
     try {
@@ -44,9 +40,9 @@ function renderParties() {
       const li = document.createElement("li");
       li.innerHTML = `
         <h2>${party.name}</h2>
+        <h2>${party.description}</h2>
         <h2>${party.date}</h2>
         <h2>${party.location}</h2>
-        <p>${party.description}</p>
         <button class="delete-button" data-party-id="${party.id}">Delete</button>
       `;
       return li;
@@ -60,6 +56,13 @@ function renderParties() {
         button.addEventListener("click", () => deleteParty(button.dataset.partyId));
     });
 }
+document.addEventListener("DOMContentLoaded", async () => {
+  await render(); // Wait for the initial rendering
+});
+
+// Select the HTML form element with the id "addPartyDetails" and assign it to the variable addPartyDetailsForm. This form is used for adding new party details.
+const addPartyDetailsForm = document.querySelector("#addPartyDetails");
+addPartyDetailsForm.addEventListener("submit", addParty);
 
 async function deleteParty(partyId) {
   try {
@@ -87,12 +90,9 @@ async function addParty(event) {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
             name: addPartyDetailsForm.name.value,
-            dates: addPartyDetailsForm.dates.value,
-        //  times: addPartyDetailsForm.times.value,
-        //  times: "2021-09-30T00:00:000Z",
-            locations: addPartyDetailsForm.locations.value,
-        //  imageUrl: addPartyDetailsForm.imageUrl.value,
             description: addPartyDetailsForm.description.value,
+            date: "2021-09-30T00:00:00.000Z",
+            location: addPartyDetailsForm.location.value,
         }),
     });
 
